21774331d4d7ddf206a589b1d0764e0526bca5be
Sedrickw <Sedrick.wall.jr@gmail.com>
2025-07-21 07:43:03 -0500

    Favorites

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -18,6 +18,7 @@ import { fetchCampsites } from "../features/campsites/campsitesSlice";
 import { fetchPromotions } from "../features/promotions/promotionsSlice";
 import { fetchComments } from "../features/comments/commentsSlice";
 import ReservationScreen from "./ReservationScreen";
+import FavoritesScreen from './FavoritesScreen';
 
 
 
@@ -114,6 +115,30 @@ const ReservationNavigator = () => {
                 headerLeft: () => (
                     <Icon 
                         name= 'tree'
+                        type = 'font-awesome'
+                        iconStyle={StyleSheet.stackIcon}
+                        onPress={() => navigation.toggleDrawer()}
+                    
+                    />
+                )
+            })}/>
+        </Stack.Navigator>
+    )
+}
+
+const FavoritesNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+             <Stack.Screen 
+                    name='Favorites'
+                    component={FavoritesScreen}
+                    options= {({navigation}) => ({
+                
+                title:'Favorites Campsite',
+                headerLeft: () => (
+                    <Icon 
+                        name= 'heart'
                         type = 'font-awesome'
                         iconStyle={StyleSheet.stackIcon}
                         onPress={() => navigation.toggleDrawer()}
@@ -254,6 +279,24 @@ const Main = () =>
                             ),
                         }}
                         />
+                    <Drawer.Screen
+                        name='Favorites'
+                        component={FavoritesNavigator}
+                        options={{
+                            title: 'My Favorites',
+                            headerShown: false,
+                            drawerIcon: ({color}) => (
+                                <Icon 
+                                    name= 'heart'
+                                    type = 'font-awesome'
+                                    size = {24}
+                                    iconStyle={{width: 24}}
+                                    color = {color}
+
+                                />
+                            ),
+                        }}
+                    />
                     <Drawer.Screen 
                         name = 'AboutNav'
                         component={AboutNavigator}
