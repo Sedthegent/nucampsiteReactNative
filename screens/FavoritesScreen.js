03da61fd79119339f79e3f6a4a8574599dea703f
Sedrickw <Sedrick.wall.jr@gmail.com>
2025-07-22 06:46:46 -0500

    Alert

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,52 +1,63 @@
 import { useSelector, useDispatch } from "react-redux";
-import { View, FlatList, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { View, FlatList, Text, TouchableOpacity, StyleSheet,Alert } from "react-native";
 import { Avatar, ListItem } from "react-native-elements";
 import Loading from "../components/LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
 import { SwipeRow } from "react-native-swipe-list-view";
-import {toggleFavorite} from '../features/favorites/favoritesSlices'
-import { use } from "react";
+import { toggleFavorite } from '../features/favorites/favoritesSlices'
 
-const FavoritesScreen = ({navigation}) => {
-    const {campsitesArray, isLoading,errMess} = useSelector(
-            (state)=>state.campsites
+
+const FavoritesScreen = ({ navigation }) => {
+    const { campsitesArray, isLoading, errMess } = useSelector(
+        (state) => state.campsites
     );
 
-    const favorites = useSelector ((state) => state.favorites);
+    const favorites = useSelector((state) => state.favorites);
     const dispatch = useDispatch();
 
-    const renderFavoriteItem = ({item: campsite}) => {
-        return(
+    const renderFavoriteItem = ({ item: campsite }) => {
+        return (
             <SwipeRow rightOpenValue={-100}>
                 <View style={styles.deleteView}>
-                    <TouchableOpacity style={styles.deleteTouchable} onPress={() => dispatch(toggleFavorite(campsite.id))}>
+                    <TouchableOpacity style={styles.deleteTouchable} onPress={() => Alert.alert('Delete Favorite?', 'Are you sure you wish to delete the favorite campsite' + campsite.name + '?',
+                        [{
+                            text: ' Cancel',
+                            onPress: () => console.log(campsite.name + 'Not Deleted'),
+                            style: 'cancel'
+                        },
+                        {
+                            text: 'OK',
+                            onPress: () => dispatch(toggleFavorite(campsite.id))
+                        }],
+                        { cancelable: false }
+                    )}>
                         <Text style={styles.deleteText}>Delete</Text>
                     </TouchableOpacity>
                 </View>
                 <View>
                     <ListItem
-                    onPress = {() => navigation.navitgate('Directory' , {
-                        screen: 'CampsiteInfo',
-                        params: (campsite)
-                    })
-                }>
-                    
-                <Avatar rounded source = {{uri:baseUrl + campsite.image}} />
-                <ListItem.Content>
-                    <ListItem.Title>{campsite.name}</ListItem.Title>
-                    <ListItem.Subtitle>{campsite.description}</ListItem.Subtitle>
-                </ListItem.Content>
-                </ListItem>
+                        onPress={() => navigation.navigate('Directory', {
+                            screen: 'CampsiteInfo',
+                            params: { campsite }
+                        })
+                        }>
+
+                        <Avatar rounded source={{ uri: baseUrl + campsite.image }} />
+                        <ListItem.Content>
+                            <ListItem.Title>{campsite.name}</ListItem.Title>
+                            <ListItem.Subtitle>{campsite.description}</ListItem.Subtitle>
+                        </ListItem.Content>
+                    </ListItem>
                 </View>
-                
+
             </SwipeRow>
         )
     }
 
-    if (isLoading){
-    return <Loading />;
+    if (isLoading) {
+        return <Loading />;
     }
-    if (errMess){
+    if (errMess) {
         return (
             <View>
                 <Text>{errMess}</Text>
@@ -54,11 +65,11 @@ const FavoritesScreen = ({navigation}) => {
         );
     }
     return (
-    <FlatList
-        data={campsitesArray.filter((campsites) =>favorites.includes(campsites.id))}
-        renderItem={renderFavoriteItem}
-        keyExtractor={(item) =>item.id.toString}
-    />
+        <FlatList
+            data={campsitesArray.filter((campsites) => favorites.includes(campsites.id))}
+            renderItem={renderFavoriteItem}
+            keyExtractor={(item) => item.id.toString()}
+        />
     );
 };
 
@@ -72,9 +83,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         flex: 1
     },
-    deleteTouchable:{
+    deleteTouchable: {
         backgroundColor: 'red',
-        heigh: '100%',
+        height: '100%',
         justifyContent: 'center'
     },
     deleteText: {
